b033058556109eab023b2a57c257116cdc6963bb
YJ <18298405849@163.com>
2017-11-30 11:15:06 +0800

    add index

--- Login.py
+++ Login.py
@@ -3,6 +3,8 @@
 from flask import Flask, request, session, redirect, url_for, render_template, make_response, jsonify
 app = Flask(__name__)
 
+# token加密解密
+
 
 @app.route('/login', methods=('POST',))
 def login():
@@ -11,6 +13,14 @@ def login():
     if username == 'root' and password == '12345':
         return jsonify({'code': 200, 'msg': 'ok', 'token': username})
     return jsonify({'code': 400, 'msg': 'error'})
+
+
+@app.route('/index')
+def index():
+    token = request.headers.get('token')
+    if token:
+        return jsonify({'code': 200, 'data': {'love': 'lp'}})
+    return jsonify({'code': 400})
 
 
 if __name__ == '__main__':
